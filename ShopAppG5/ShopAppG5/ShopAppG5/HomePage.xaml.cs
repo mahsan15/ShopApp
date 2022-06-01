a52390007f7a54ceaf023273bada2af597239ce9
mahsan15 <82739557+mahsan15@users.noreply.github.com>
2022-06-01 12:33:24 -0400

    Add files via upload

---
+++ ShopAppG5/ShopAppG5/ShopAppG5/HomePage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShopAppG5
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+
+            PriceFilter.Items.Add("Less than $10");
+            PriceFilter.Items.Add("$10 - $25");
+            PriceFilter.Items.Add("$25 - $50");
+            PriceFilter.Items.Add("$50 - $100");
+            PriceFilter.Items.Add("$100 - $250");
+            PriceFilter.Items.Add("$250 - $500");
+            PriceFilter.Items.Add("$500 - $1,000");
+            PriceFilter.Items.Add("More than $1,000");
+        }
+    }
+}
